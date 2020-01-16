78cebb1e265e820442de489e0bdf922e8ce424b8
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-16 12:37:22 -1000

    Finished with resolved corrections

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,49 +5,50 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
+function _sumFibs(maxFibValue) {
   let sum = 0;
-  
-  // fibonacci
-  let testArr = [0, 1];
-  for (let i = 2; i < maxFibValue + 1; i++){
-    testArr.push(testArr[i - 2] + testArr[i -1])
-  } // end of fibonacci
-  
-  for(i=0; testArr[i]<=maxFibValue; i++){
-    if(testArr[i]%2 === 0){ // check if even
-      sum += testArr[i]; // add that even value
+  let prev = 0;
+  let curr = 1;
+  let next;
+
+  for (let i = 2; curr <= maxFibValue + 1; i++) {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+
+    if (curr % 2 === 0) {
+      sum += curr;
     }
   }
 
   return sum;
 }
 
-
 // bonus round
-function _highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber(maxFibValue) {
   let highest = 0;
 
   //define your base case, validate your input
 
-    // fibonacci
-    let testArr = [0, 1];
-    for (let i = 2; i < maxFibValue + 1; i++){
-      testArr.push(testArr[i - 2] + testArr[i -1])
-    } // end of fibonacci
-    
-    for(i=0; testArr[i]<=maxFibValue; i++){
-      highest = testArr[i];
-    }
+  let prev = 0;
+  let curr = 1;
+  let next;
+
+  for (let i = 2; curr <= maxFibValue + 1; i++) {
+    next = prev + curr;
+    prev = curr;
+    highest = curr;
+    curr = next;
+  }
 
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
